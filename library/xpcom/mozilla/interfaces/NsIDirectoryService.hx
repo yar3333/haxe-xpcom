71ca5de8e160d9f93cc4fa3ae0c287177c6333d5
Yaroslav Sivakov <Yaroslav Sivakov@localhost>
2014-08-09 00:47:24 +0400

    * minor

--- library/xpcom/mozilla/interfaces/NsIDirectoryService.hx
+++ library/xpcom/mozilla/interfaces/NsIDirectoryService.hx
@@ -5,6 +5,13 @@ import xpcom.mozilla.objects.*;
 
 @:native("Components.interfaces.nsIDirectoryService") extern class NsIDirectoryService extends NsISupports
 {
+	public static inline function getService() : NsIDirectoryService
+	{
+		var r : NsIDirectoryService = xpcom.Components.classes[cast "@mozilla.org/file/directory_service;1"].getService(NsIDirectoryService);
+		r.init();
+		return r;
+	}
+	
 	function init() : Void;
 	function registerProvider(prov:NsIDirectoryServiceProvider) : Void;
 	function unregisterProvider(prov:NsIDirectoryServiceProvider) : Void;
