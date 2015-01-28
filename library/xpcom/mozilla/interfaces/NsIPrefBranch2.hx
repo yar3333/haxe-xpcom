f396dbc46a2667ac6eee30be853b27935a9262ea
Yaroslav Sivakov <yar3333@gmail.com>
2015-01-28 19:31:49 +0300

    - nsIPrefBranch2 (deprecated)

--- library/xpcom/mozilla/interfaces/NsIPrefBranch2.hx
+++
@@ -1,8 +0,0 @@
-package xpcom.mozilla.interfaces;
-
-import xpcom.types.*;
-import xpcom.mozilla.objects.*;
-
-@:native("Components.interfaces.nsIPrefBranch2") extern class NsIPrefBranch2 extends NsIPrefBranch
-{
-}
