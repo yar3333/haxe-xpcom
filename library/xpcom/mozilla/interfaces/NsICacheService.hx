b6a91aec496f475555896f54d60ee290a60ffc76
Yaroslav Sivakov <yar3333@gmail.com>
2015-07-01 14:51:34 +0300

    * nsICacheService fix

--- library/xpcom/mozilla/interfaces/NsICacheService.hx
+++ library/xpcom/mozilla/interfaces/NsICacheService.hx
@@ -3,8 +3,16 @@ package xpcom.mozilla.interfaces;
 import xpcom.types.*;
 import xpcom.mozilla.objects.*;
 
+/**
+ * Removed from Firefox 38. Use nsICacheStorageService instead.
+ */
 @:native("Components.interfaces.nsICacheService") extern class NsICacheService extends NsISupports
 {
+	public static inline function getService() : NsICacheService
+	{
+		return xpcom.Components.classes[cast "@mozilla.org/network/cache-service;1"].getService(NsICacheService);
+	}
+	
 	function createSession(clientID:String, storagePolicy:NsCacheStoragePolicy, streamBased:Bool) : NsICacheSession;
 	function createTemporaryClientID(storagePolicy:NsCacheStoragePolicy) : ACString;
 	function evictEntries(storagePolicy:NsCacheStoragePolicy) : Void;
