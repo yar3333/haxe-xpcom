49b540409004f38cb364aa81f1ccca0ba26caeae
Yaroslav Sivakov <yar3333@gmail.com>
2015-07-31 19:03:37 +0300

    * HttpServer: Haxe 3.2.0 support

--- library/xpcom/mozilla/HttpServer.hx
+++ library/xpcom/mozilla/HttpServer.hx
@@ -9,7 +9,7 @@ class HttpServer
 	
 	public static function createInstance(?dump:String->Void) : xpcom.mozilla.interfaces.NsIHttpServer
 	{
-		untyped __js__("if (typeof xpcom_mozilla_HttpServer == 'undefined') window.xpcom_mozilla_HttpServer = new xpcom_mozilla_HttpServerComponent(dump).HttpServer;");
-		return untyped __js__("new xpcom_mozilla_HttpServer()");
+		untyped __js__("if (typeof xpcom_mozilla_HttpServer_Class == 'undefined') window.xpcom_mozilla_HttpServer_Class = (new xpcom_mozilla_HttpServerComponent(dump)).HttpServer");
+		return untyped __js__("new xpcom_mozilla_HttpServer_Class()");
 	}
 }
